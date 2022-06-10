14888696f8623ed18f9ca0688b8f10ca3238d778
IrfanParic <irfan.paric@stu.ssst.edu.ba>
2022-06-10 02:06:32 +0200

    Added: Header, introduction and about me section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    AOS.init();
+  }
+
+  scroll(div: HTMLDivElement) {
+    div.scrollIntoView();
+  }
+
+  scrolltop() {
+    window.scroll(0,0)
+  }
+}
