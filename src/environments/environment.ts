efe858a82c065b62179721649c0740b1fc4a8156
IrfanParic <irfan.paric@stu.ssst.edu.ba>
2022-10-23 12:46:45 +0200

    Fixed: environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  backendUrl: "https://irfanparic.herokuapp.com",
+  backendUrl: "https://personalcv-backend-production.up.railway.app",
 };
 
 /*
