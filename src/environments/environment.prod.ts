2e88b7d73148fa81ff18d964e76d335328161595
IrfanParic <irfan.paric@stu.ssst.edu.ba>
2023-01-16 19:26:30 +0100

    Fixed: bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: "https://personalcv-backend-production.up.railway.app",
+  backendUrl: "https://personalcv-backend-production.up.railway.app/projects",
 };
